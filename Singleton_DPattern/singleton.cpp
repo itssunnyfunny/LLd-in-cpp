6d6e772ae01904f6d9d7148aa5a9af25a6c64857
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-28 15:55:35 +0530

    singleton design pattern

---
+++ Singleton_DPattern/singleton.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class Singleton {
+    private: 
+    static Singleton* instance;
+    // Singleton() {}
+    // public:
+    // static Singleton* getInstance() {
+    //     if (instance == nullptr) {
+    //         instance = new Singleton();
+    //     }
+    //     return instance;
+    // }
+
+    Singleton() {
+        cout << "Singleton constructor called" << endl;
+    }
+
+     public: 
+       static Singleton * getInstance() {
+        return instance;
+       }
+};
+
+
+Singleton* Singleton::instance = new Singleton();
+
+int main() {
+
+    Singleton *s1 = Singleton::getInstance();
+    Singleton *s2 = Singleton::getInstance();
+
+    cout << (s1 == s2) << endl;
+    return 0;
+}
